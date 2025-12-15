9f309ad804fe233aa82e6e85c4ad92ce69759df5
arhamshafi <arhambutt2923@gmail.com>
2025-12-15 23:16:31 +0500

    try to work on clerk but there is an error about catchallroutes

---
+++ src/app/(auth)/signup/page.js
@@ -0,0 +1,10 @@
+import { SignUp } from '@clerk/nextjs'
+import React from 'react'
+
+export default function page() {
+    return (
+        <div className='w-full min-h-screen bg-black flex justify-center items-center'>
+            <SignUp />
+        </div>
+    )
+}
---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-full min-h-screen bg-black flex justify-center items-center text-white">
+      HOME PAGE
+    </div>
+  );
+}
